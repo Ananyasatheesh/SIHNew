1fc625fe55ecc54d07cba95093f9657ca2f40cc0
Ananyasatheesh <93508074+Ananyasatheesh@users.noreply.github.com>
2023-10-11 20:58:41 +0530

    Add files via upload

---
+++ src/components/Content.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import TextField from "@mui/material/TextField"
+
+const Content = () => {
+  return (
+    <div>
+      <TextField
+          id="outlined-basic"
+          variant="outlined"
+          fullWidth
+          label="Search"
+        />
+    </div>
+  )
+}
+
+export default Content
